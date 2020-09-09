582eabfdd21392bdd1fd644a77f0949e64aa6b28
Foxcc6 <1065318755@qq.com>
2020-09-09 21:31:04 +0800

    vue-music

---
+++ vue/vueMusic/vue-music/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+    <Header>
+      <i class="iconfont" slot="left-icon">&#xe96d;</i>
+      <span slot="content">我的音乐</span>
+      <i class="iconfont" slot="right-icon">&#xe67a;</i>
+    </Header>
+    <tab></tab>
+
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+
+    <!-- menu -->
+    <sideBar></sideBar>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import sideBar from '@/components/sideBar'
+import tab from '@/components/tab'
+export default {
+    components:{
+      Header,
+      sideBar,
+      tab
+    }
+}
+</script>
+
+<style lang="stylus">
+@font-face {
+  font-family: 'iconfont';  /* project id 2052915 */
+  src: url('//at.alicdn.com/t/font_2052915_qfcub9o8kc.eot');
+  src: url('//at.alicdn.com/t/font_2052915_qfcub9o8kc.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_2052915_qfcub9o8kc.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_2052915_qfcub9o8kc.woff') format('woff'),
+  url('//at.alicdn.com/t/font_2052915_qfcub9o8kc.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_2052915_qfcub9o8kc.svg#iconfont') format('svg');
+}
+.iconfont{
+    font-family:"iconfont" !important;
+    font-size:16px;font-style:normal;
+    -webkit-font-smoothing: antialiased;
+    -webkit-text-stroke-width: 0.2px;
+    -moz-osx-font-smoothing: grayscale;}
+    html,
+body {
+  line-height: 1;
+  font-family: PingFang SC, STHeitiSC-Light, Helvetica-Light, arial, sans-serif;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  background: rgba(8, 5, 58, 0.9);
+  color: #fff;
+}
+</style>  
