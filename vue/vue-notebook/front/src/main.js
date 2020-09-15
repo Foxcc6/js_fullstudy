bb1880164532f6e479d87ad02d416b16954bff30
Foxcc6 <1065318755@qq.com>
2020-09-15 19:02:15 +0800

    notebook

--- vue/vue-notebook/front/src/main.js
+++ vue/vue-notebook/front/src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
 
+import 'vant/lib/index.css';
+import { Swipe, SwipeItem, Icon, Field, ActionSheet, Toast, Panel, Uploader } from 'vant';
+
+Vue.use(Swipe).use(SwipeItem).use(Icon).use(Field).use(ActionSheet).use(Toast).use(Panel).use(Uploader)
+
+router.beforeEach((to,from,next)=>{
+  document.title = to.meta.title
+  next()
+})
 Vue.config.productionTip = false
 
 new Vue({
--- vue/vue-notebook/note-book/routes/users.js
+++ vue/vue-notebook/note-book/routes/users.js
@@ -3,18 +3,19 @@ const userService = require('../controllers/mysqlConfig')
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
 
 router.get('/all',async function(ctx,next){
   await userService.getAllUsers().then((res)=>{
     ctx.body = res
   })
+})
+
+//登录注册
+router.post('/userRegister',async(ctx,next) =>{
+  var _username = ctx.request.body._username
+  var _userpwd = ctx.request.body._userpwd
+  var _nickname = ctx.request.body._nickname
 })
 
 module.exports = router
